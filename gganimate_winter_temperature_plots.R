16568face04108aa596d9056b0aa548d0598ff37
Bénédicte Wenden <37516758+bwenden@users.noreply.github.com>
2018-08-29 09:05:22 +0200

    Update gganimate_winter_temperature_plots.R

--- gganimate_winter_temperature_plots.R
+++ gganimate_winter_temperature_plots.R
@@ -6,22 +6,46 @@ winter <- read.csv2("winter.csv") #Import annual winter temperatures 188-2018, d
 wr <- map_data("world") #World map
 
 ##Mean winter temperature
-wrmap <- ggplot(winter, aes(x,y, fill=layer)) + 
+wrmap <- ggplot(winter[winter$Year==2017,], aes(x,y, fill=layer)) + 
   geom_raster()+
   geom_polygon(data=wr,aes(x = long, y = lat,group=group),fill = NA, colour = "black") +
   theme_bw() + 
-  coord_equal()
+  theme(legend.title=element_text(size=12),
+        legend.text = element_text(size=12),
+        axis.title=element_blank(),
+        axis.text = element_blank(),
+        axis.ticks=element_blank(),
+        strip.text = element_text(size=14),
+        plot.title = element_text(size = 18)) +
+  scale_fill_gradient2(midpoint=0, low="blue", mid="white", high="red")+
+  coord_equal() + 
+  labs(title="Winter temperature anomalies - 2017",
+       subtitle="Comparison with 1951-1980 period",
+       fill="Anomalies (°C)",
+       caption="GISS Surface Temperature Analysis (GISTEMP)")
 plot(wrmap)
 
 ###Animated plots with mean annual winter temperatures 1880-2017
 p <- ggplot(winter, aes(x,y, fill=layer)) + 
   geom_raster(interpolate=TRUE)+
   geom_polygon(data=wr,aes(x = long, y = lat,group=group),fill = NA, colour = "black") +
-  theme_bw() + scale_fill_gradient2(midpoint=0, low="blue", mid="white", high="red")+
+  theme_bw() + 
+  ggtitle("Winter temperature anomalies - 1880-2017")+
+  theme(legend.title=element_text(size=12),
+        legend.text = element_text(size=12),
+        axis.title=element_blank(),
+        axis.text = element_blank(),
+        axis.ticks=element_blank(),
+        strip.text = element_text(size=14),
+        plot.title = element_text(size = 18)) +
+  scale_fill_gradient2(midpoint=0, low="blue", mid="white", high="red")+
   coord_equal() + 
-  labs(title = 'Year: {frame_time}') +
+  labs(title = 'Year: {frame_time}',
+       subtitle="Comparison with 1951-1980 period",
+       fill="Anomalies (°C)",
+       caption="GISS Surface Temperature Analysis (GISTEMP)") +
   transition_time(Year) +
   ease_aes('linear')
 
 animate(p, nframes=138, fps=2, height=500,width=625)
-anim_save(p, 'winter_temp_anomalies.gif)
+anim_save(p, 'winter_temp_anomalies.gif')
